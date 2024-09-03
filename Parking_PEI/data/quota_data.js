97b3585160b59318104bc7f936e07fac30780afd
lexuanbinh <lb@silverliningresearch.com>
2024-09-03 18:19:17 +0700

    update new files

---
+++ Parking_PEI/data/quota_data.js
@@ -0,0 +1,13 @@
+     let quota_info = ` 
+[
+{"quota_id": "P43", "Area": "Area", "Quota": "50"},
+{"quota_id": "P51", "Area": "Economy Parken", "Quota": "50"},
+{"quota_id": "P3", "Area": "Economy Parken", "Quota": "70"},
+{"quota_id": "P20", "Area": "Terminal Parken", "Quota": "110"},
+{"quota_id": "P26", "Area": "Terminal Parken", "Quota": "70"},
+{"quota_id": "T1C", "Area": "Terminal Parken", "Quota": "50"},
+{"quota_id": "T2", "Area": "Terminal drive ways (Vorfahrten)", "Quota": "50"}
+
+]    
+
+ `;

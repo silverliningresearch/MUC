153c2a3ea20d62b8d0e23f8ff287e2a9339a99ef
lexuanbinh <lb@silverliningresearch.com>
2026-01-04 16:55:27 +0700

    update new files

--- PEIAN/scripts/CalculateDataArrival.js
+++ PEIAN/scripts/CalculateDataArrival.js
@@ -166,14 +166,14 @@ function PreparaArrivalData() {
   initCurrentTimeVars();
 
   quota_data = [
-    { "Location": "T1-A/E03", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},
-    { "Location": "T1-A/E04", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},
-    // { "Location": "T1-B", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},,
-    { "Location": "T1-C", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},
-    { "Location": "T1-D", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},
-    { "Location": "T1-E", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},
-    { "Location": "T2", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0},  
-    { "Location": "Total", "Exit": "0", "Belt" : "0", "Landside" :"0", "Total" : 0}       
+    { "Location": "T1-A/E03", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},
+    { "Location": "T1-A/E04", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},
+    // { "Location": "T1-B", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},,
+    { "Location": "T1-C", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},
+    { "Location": "T1-D", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},
+    { "Location": "T1-E", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},
+    { "Location": "T2", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0},  
+    { "Location": "Total", "Exit": "0%", "Belt" : "0%", "Landside" :"0%", "Total" : 0}       
 ];
 
   for (i = 0; i < location_percent_data.length; i++) {
